c2f4118a1ae86072b2ca9ac010fb033a03187ab2
ryanmaguire <rmaguire@protonmail.com>
2024-06-13 18:56:27 -0400

    init for python wrapper.

---
+++ tmpl_experiments/__init__.py
@@ -0,0 +1,25 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl_experiments.                                  #
+#                                                                              #
+#   libtmpl_experiments is free software: you can redistribute it and/or       #
+#   modify it under the terms of the GNU General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl_experiments is distributed in the hope that it will be useful,     #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License along    #
+#   with libtmpl_experiments.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+"""
+from . import arccos
+from . import arcsin
+from . import besseli0
+from . import erf
+from . import fresnel
